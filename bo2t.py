d350d0484a09aec4180801ce1c0c0185c95196f1
cryptunex <122434404+cryptunex@users.noreply.github.com>
2023-01-18 14:16:26 +0300

    Update bo2t.py

--- bo2t.py
+++ bo2t.py
@@ -1,20 +1,22 @@
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-TOKEN = "5747207857:AAH0F8fGDmXFfmGAkA7A-lKXhVhoS_elAc8"
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+import telebot
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(msg: types.Message):
-    await msg.reply_to_message('Я бот. Приятно познакомиться')
+# настройка
+bot = telebot.TeleBot('5747207857:AAH0F8fGDmXFfmGAkA7A-lKXhVhoS_elAc8')
 
-@dp.message_handler(content_types=['text'])
-async def get_text_messages(msg: types.Message):
-   if msg.text.lower() == 'привет':
-       await msg.answer('Привет!')
-   else:
-       await msg.answer('Не понимаю, что это значит.')
+# функции
+@bot.message_handler(commands=['start'])
+def start(message):
 
-if __name__ == '__main__':
-   executor.start_polling(dp)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Скриншот")
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "Хулиганим", reply_markup=markup)
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+
+    if message.text == 'Скриншот':
+	ot.send_message(message.from_user.id, "Хулиганим", reply_markup=markup)
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True, interval=0)
