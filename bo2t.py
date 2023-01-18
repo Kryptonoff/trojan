252d9397c66931f6ae29ae294086ef0aa19fa4fc
cryptunex <122434404+cryptunex@users.noreply.github.com>
2023-01-18 14:20:14 +0300

    Update bo2t.py

--- bo2t.py
+++ bo2t.py
@@ -1,4 +1,6 @@
 import telebot
+import pyautogui
+from telebot import types
 
 # настройка
 bot = telebot.TeleBot('5747207857:AAH0F8fGDmXFfmGAkA7A-lKXhVhoS_elAc8')
@@ -6,6 +8,7 @@ bot = telebot.TeleBot('5747207857:AAH0F8fGDmXFfmGAkA7A-lKXhVhoS_elAc8')
 # функции
 @bot.message_handler(commands=['start'])
 def start(message):
+
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Скриншот")
     markup.add(btn1)
@@ -13,8 +16,10 @@ def start(message):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+
     if message.text == 'Скриншот':
-	bot.send_message(message.from_user.id, "Хулиганим")
+        pyautogui.screenshot('screenshot.png',region=(0,0, 1920, 1080))
+        bot.send_photo(message.from_user.id, photo=open('screenshot.png', 'rb'))
 
 if __name__ == "__main__":
     bot.polling(none_stop=True, interval=0)
