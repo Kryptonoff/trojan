2648e0467472aedacba64b71e66c5fdb27052f34
cryptunex <122434404+cryptunex@users.noreply.github.com>
2023-01-18 13:49:53 +0300

    Update bo2t.py

--- bo2t.py
+++ bo2t.py
@@ -1,13 +1,10 @@
 import telebot
-bot = telebot.TeleBot('5747207857:AAH0F8fGDmXFfmGAkA7A-lKXhVhoS_elAc8')
 
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-  if message.text == "Привет":
-      bot.send_message(message.from_user.id, "Привет, сейчас я расскажу тебе гороскоп на сегодня.")
-  elif message.text == "/help":
-      bot.send_message(message.from_user.id, "Напиши Привет")
-  else:
-      bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
-      
+bot = telebot.TeleBot('5747207857:AAH0F8fGDmXFfmGAkA7A-lKXhVhoS_elAc8')
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
 bot.polling(none_stop=True, interval=0)
