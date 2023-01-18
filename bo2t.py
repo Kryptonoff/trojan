386f9f94fd7d09ea5ae3df9d52da85727de43a42
cryptunex <122434404+cryptunex@users.noreply.github.com>
2023-01-18 14:18:17 +0300

    Update bo2t.py

--- bo2t.py
+++ bo2t.py
@@ -6,7 +6,6 @@ bot = telebot.TeleBot('5747207857:AAH0F8fGDmXFfmGAkA7A-lKXhVhoS_elAc8')
 # функции
 @bot.message_handler(commands=['start'])
 def start(message):
-
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Скриншот")
     markup.add(btn1)
@@ -14,9 +13,8 @@ def start(message):
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
     if message.text == 'Скриншот':
-	ot.send_message(message.from_user.id, "Хулиганим", reply_markup=markup)
+	bot.send_message(message.from_user.id, "Хулиганим")
 
 if __name__ == "__main__":
     bot.polling(none_stop=True, interval=0)
